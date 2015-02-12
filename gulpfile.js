36e197104dad2cae1eb3eefa85f6979fb11463e4
Falk Hoppe <falkhoppe81@gmail.com>
2015-02-12 11:17:45 +0100

    added gulp stack for future frontend stack

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+/*jslint devel: false, node: true, indent: 2, maxlen: 120 */
+
+var
+  gulp = require('gulp'),
+  gutil = require('gulp-util');
+
+var
+  stylus = require('stylus'),
+  koutoSwiss = require('kouto-swiss');
