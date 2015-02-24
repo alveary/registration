83772d40a75fd530103a96ac9c6091e01d0d01d3
Falk Hoppe <falkhoppe81@gmail.com>
2015-02-24 19:40:48 +0100

    fixed post test

--- registration_test.go
+++ registration_test.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/go-martini/martini"
@@ -37,6 +38,24 @@ func (suite *RegistrationTestSuite) TestIfFormContainsRegistrationFields() {
 	assert.True(suite.T(), doc.Exists(".test-registration-form"), "Should have a form on it rendered")
 	assert.True(suite.T(), doc.Exists("[name=firstname]"), "Should have a field for firstname")
 	assert.True(suite.T(), doc.Exists("[name=lastname]"), "Should have a field for firstname")
+}
+
+func (suite *RegistrationTestSuite) TestCreatingANewRegistration() {
+	req, _ := http.NewRequest("POST", "/", strings.NewReader(`lastname=hoppe&email=falk.hoppe@innoq.com`))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+
+	w := httptest.NewRecorder()
+
+	suite.app.ServeHTTP(w, req)
+	doc, err := goquery.NewDocumentFromReader(w.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	assert.True(suite.T(), doc.Find(".test-registration-form [name=firstname]").Parent().HasClass("error"), "Should have a form on it rendered")
+	assert.False(suite.T(), doc.Find(".test-registration-form [name=lastname]").Parent().HasClass("error"), "Should have a form on it rendered")
+	assert.False(suite.T(), doc.Find(".test-registration-form [name=email]").Parent().HasClass("error"), "Should have a form on it rendered")
 }
 
 // In order for 'go test' to run this suite, we need to create
