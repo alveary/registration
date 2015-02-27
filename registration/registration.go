50b860c16b454a9626d8467b17acc9338d51bad6
Falk Hoppe <falkhoppe81@gmail.com>
2015-02-27 17:37:50 +0100

    mvd registration context to lib

---
+++ registration/registration.go
@@ -0,0 +1,65 @@
+package registration
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"net/http"
+	"time"
+
+	"github.com/jamieomatthews/validation"
+	"github.com/martini-contrib/binding"
+)
+
+// Registration is the minimal information a user has to provide
+type Registration struct {
+	Email    string `form:"email" json:"email" binding:"required"`
+	Password string `form:"password" json:"password" binding:"required"`
+}
+
+// Validate password of a given registration
+func (registration Registration) Validate(errors binding.Errors, req *http.Request) binding.Errors {
+	v := validation.NewValidation(&errors, registration)
+
+	v.Validate(&registration.Email).Classify("email-class").Email()
+	v.Validate(&registration.Password).Key("password").MinLength(9)
+
+	return *v.Errors.(*binding.Errors)
+}
+
+// RequestRegistration ...
+func (registration Registration) RequestRegistration() (target string, err error) {
+	failure := make(chan error)
+	success := make(chan string)
+	defer func() {
+		close(failure)
+		close(success)
+	}()
+
+	go func() {
+		json, err := json.Marshal(registration)
+		if err != nil {
+			failure <- err
+			return
+		}
+
+		// TODO: handle target
+		_, requestErr := http.Post("http://localhost:9001/", "application/json", bytes.NewBuffer(json))
+
+		if requestErr != nil {
+			failure <- requestErr
+			return
+		}
+
+		success <- "target"
+	}()
+
+	select {
+	case result := <-success:
+		return result, nil
+	case exception := <-failure:
+		return "", exception
+	case <-time.After(time.Second * 3):
+		return "", errors.New("timeout")
+	}
+}
--- user_registration.go
+++ user_registration.go
@@ -3,28 +3,20 @@ package main
 import (
 	"bytes"
 	"encoding/json"
-	"errors"
 	"flag"
 	"fmt"
 	"net/http"
 	"strconv"
-	"time"
 
+	"github.com/alveary/user-registration/registration"
 	"github.com/go-martini/martini"
-	validation "github.com/jamieomatthews/validation"
 	"github.com/martini-contrib/binding"
 	"github.com/martini-contrib/render"
 )
 
-// Registration is the minimal information a user has to provide
-type Registration struct {
-	Email    string `form:"email" json:"email" binding:"required"`
-	Password string `form:"password" json:"password" binding:"required"`
-}
-
 // Result is a combination of a given registration and the related errors
 type Result struct {
-	Registration Registration
+	Registration registration.Registration
 	Errors       map[string]string
 }
 
@@ -40,53 +32,6 @@ func errorMap(errors binding.Errors) map[string]string {
 	return errorMap
 }
 
-// Validate password of a given registration
-func (reg Registration) Validate(errors binding.Errors, req *http.Request) binding.Errors {
-	v := validation.NewValidation(&errors, reg)
-
-	v.Validate(&reg.Email).Classify("email-class").Email()
-	v.Validate(&reg.Password).Key("password").MinLength(9)
-
-	return *v.Errors.(*binding.Errors)
-}
-
-// RequestRegistration ...
-func RequestRegistration(registration Registration, r render.Render) (target string, err error) {
-	failure := make(chan error)
-	success := make(chan string)
-	defer func() {
-		close(failure)
-		close(success)
-	}()
-
-	go func() {
-		json, err := json.Marshal(registration)
-		if err != nil {
-			failure <- err
-			return
-		}
-
-		// TODO: handle target
-		_, requestErr := http.Post("http://localhost:9001/", "application/json", bytes.NewBuffer(json))
-
-		if requestErr != nil {
-			failure <- requestErr
-			return
-		}
-
-		success <- "target"
-	}()
-
-	select {
-	case result := <-success:
-		return result, nil
-	case exception := <-failure:
-		return "", exception
-	case <-time.After(time.Second * 3):
-		return "", errors.New("timeout")
-	}
-}
-
 // AppEngine for web engine setup
 func AppEngine() *martini.ClassicMartini {
 	m := martini.Classic()
@@ -100,7 +45,7 @@ func AppEngine() *martini.ClassicMartini {
 		resp.WriteHeader(http.StatusOK)
 	})
 
-	m.Post("/", binding.Form(Registration{}), func(r render.Render, errors binding.Errors, registration Registration, resp http.ResponseWriter) {
+	m.Post("/", binding.Form(registration.Registration{}), func(r render.Render, errors binding.Errors, registration registration.Registration, resp http.ResponseWriter) {
 		if errors.Len() > 0 {
 
 			errorMap := errorMap(errors)
@@ -108,7 +53,7 @@ func AppEngine() *martini.ClassicMartini {
 
 		} else {
 
-			target, err := RequestRegistration(registration, r)
+			target, err := registration.RequestRegistration()
 			fmt.Println(err)
 
 			if err != nil {
