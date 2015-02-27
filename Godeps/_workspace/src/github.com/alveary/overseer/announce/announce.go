589d4e07e5166878dbf6c973acc7b9b3f13f38e0
Falk Hoppe <falkhoppe81@gmail.com>
2015-02-27 22:52:18 +0100

    updated dependencies

--- Godeps/_workspace/src/github.com/alveary/overseer/announce/announce.go
+++ Godeps/_workspace/src/github.com/alveary/overseer/announce/announce.go
@@ -6,11 +6,55 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 )
+
+func requestServiceAnnouncement(overseerRoot string, service []byte) {
+	go func() {
+		available := true
+
+		for available {
+
+			go func() {
+				checkchan := make(chan bool)
+				errorchan := make(chan error)
+				defer func() {
+					close(checkchan)
+					close(errorchan)
+				}()
+
+				go func() {
+					resp, err := http.Post(overseerRoot, "application/json", bytes.NewBuffer(service))
+					if err != nil {
+						errorchan <- err
+						return
+					}
+					if resp.StatusCode > 299 {
+						errorchan <- fmt.Errorf("Request Error: %s", resp.Status)
+						return
+					}
+
+					checkchan <- true
+				}() // end request routine
+
+				select {
+				case <-checkchan:
+					// JUST GO ON
+				case <-errorchan:
+				case <-time.After(time.Second * 3):
+				}
+
+				time.Sleep(10 * time.Minute) // update registration once a minute
+			}() // end wait and handle routine (chans are cleaned here)
+
+		} // end for loop
+
+	}() // end loop routine
+}
 
 // NewService provides a method to attach a new Service to the overseer stack
 func NewService(serviceName string, serviceRoot string, aliveResource string) {
-	json, _ := json.Marshal(struct {
+	service, _ := json.Marshal(struct {
 		serviceName   string
 		serviceRoot   string
 		aliveResource string
@@ -27,5 +71,5 @@ func NewService(serviceName string, serviceRoot string, aliveResource string) {
 		return
 	}
 
-	http.Post(overseerRoot, "application/json", bytes.NewBuffer(json))
+	requestServiceAnnouncement(overseerRoot, service)
 }
